f097cc5c57ff4bd592b00608393f8f5262890309
mahiro0622 <237707408+hakun0205-svg@users.noreply.github.com>
2026-07-29 15:51:57 +0900

    Update myExtension.js

--- myExtension.js
+++ myExtension.js
@@ -1,124 +1,70 @@
-class WebSocketExtension {
+class ChatExtension {
   constructor(runtime) {
     this.runtime = runtime;
     this.ws = null;
     this.lastMessage = '';
-    this.isConnected = false;
+    this.chatLog = [];
   }
 
   getInfo() {
     return {
-      id: 'websocket',
-      name: 'WebSocket',
+      id: 'chatws',
+      name: 'Chat WebSocket',
       blocks: [
         {
-          opcode: 'connect',
+          opcode: 'connectChat',
           blockType: Scratch.BlockType.COMMAND,
-          text: 'connect to [URL]',
+          text: 'チャットに接続 [URL]',
           arguments: {
             URL: { type: Scratch.ArgumentType.STRING, defaultValue: 'ws://localhost:8080' }
           }
         },
         {
-          opcode: 'disconnect',
+          opcode: 'sendChat',
           blockType: Scratch.BlockType.COMMAND,
-          text: 'disconnect'
-        },
-        {
-          opcode: 'send',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'send [MESSAGE]',
+          text: 'チャットに送信 [MESSAGE]',
           arguments: {
             MESSAGE: { type: Scratch.ArgumentType.STRING }
           }
         },
         {
-          opcode: 'sendJSON',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'send JSON [OBJ]',
-          arguments: {
-            OBJ: { type: Scratch.ArgumentType.STRING, defaultValue: '{"hello":"world"}' }
-          }
+          opcode: 'getLastChat',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '最新のチャットメッセージ'
         },
         {
-          opcode: 'onMessage',
+          opcode: 'getChatLog',
           blockType: Scratch.BlockType.REPORTER,
-          text: 'last message'
-        },
-        {
-          opcode: 'onJSON',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'last JSON'
-        },
-        {
-          opcode: 'status',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'connection status'
+          text: 'チャットログ'
         }
       ]
     };
   }
 
-  connect(args) {
+  connectChat(args) {
     this.ws = new WebSocket(args.URL);
     this.lastMessage = '';
-    this.lastJSON = '';
-    this.isConnected = false;
-
-    this.ws.onopen = () => {
-      this.isConnected = true;
-    };
-
-    this.ws.onclose = () => {
-      this.isConnected = false;
-    };
+    this.chatLog = [];
 
     this.ws.onmessage = (event) => {
       this.lastMessage = event.data;
-
-      try {
-        this.lastJSON = JSON.stringify(JSON.parse(event.data));
-      } catch {
-        this.lastJSON = '';
-      }
+      this.chatLog.push(event.data);
     };
   }
 
-  disconnect() {
-    if (this.ws) {
-      this.ws.close();
-      this.isConnected = false;
-    }
-  }
-
-  send(args) {
+  sendChat(args) {
     if (this.ws && this.ws.readyState === WebSocket.OPEN) {
       this.ws.send(args.MESSAGE);
     }
   }
 
-  sendJSON(args) {
-    if (this.ws && this.ws.readyState === WebSocket.OPEN) {
-      try {
-        const obj = JSON.parse(args.OBJ);
-        this.ws.send(JSON.stringify(obj));
-      } catch {
-        console.warn('Invalid JSON');
-      }
-    }
-  }
-
-  onMessage() {
+  getLastChat() {
     return this.lastMessage || '';
   }
 
-  onJSON() {
-    return this.lastJSON || '';
-  }
-
-  status() {
-    return this.isConnected ? 'connected' : 'disconnected';
+  getChatLog() {
+    return JSON.stringify(this.chatLog);
   }
 }
 
-Scratch.extensions.register(new WebSocketExtension());
+Scratch.extensions.register(new ChatExtension());
