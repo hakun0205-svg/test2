c0fc5ee8a9e97fa85de0a9df1391f14a254f18a4
mahiro0622 <237707408+hakun0205-svg@users.noreply.github.com>
2026-07-29 15:42:07 +0900

    Update myExtension.js

--- myExtension.js
+++ myExtension.js
@@ -1,19 +1,66 @@
 class WebSocketExtension {
   constructor() {
     this.ws = null;
+    this.lastMessage = "";
   }
 
-  connect({URL}) {
+  getInfo() {
+    return {
+      id: "websocket",
+      name: "WebSocket",
+      blocks: [
+        {
+          opcode: "connect",
+          blockType: "command",
+          text: "connect to [URL]",
+          arguments: {
+            URL: {
+              type: "string",
+              defaultValue: "ws://localhost:8080"
+            }
+          }
+        },
+        {
+          opcode: "send",
+          blockType: "command",
+          text: "send [MSG]",
+          arguments: {
+            MSG: {
+              type: "string",
+              defaultValue: "hello"
+            }
+          }
+        },
+        {
+          opcode: "lastMessage",
+          blockType: "reporter",
+          text: "last received message"
+        }
+      ]
+    };
+  }
+
+  connect({ URL }) {
     this.ws = new WebSocket(URL);
-    this.ws.onopen = () => console.log("connected");
-    this.ws.onmessage = (msg) => console.log("received:", msg.data);
+    this.ws.onopen = () => console.log("Connected:", URL);
+    this.ws.onmessage = (msg) => {
+      this.lastMessage = msg.data;
+      console.log("Received:", msg.data);
+    };
+    this.ws.onerror = (err) => console.error("WebSocket error:", err);
   }
 
-  send({MSG}) {
+  send({ MSG }) {
     if (this.ws && this.ws.readyState === WebSocket.OPEN) {
       this.ws.send(MSG);
+    } else {
+      console.warn("WebSocket is not connected.");
     }
+  }
+
+  lastMessage() {
+    return this.lastMessage;
   }
 }
 
-module.exports = WebSocketExtension;
+Scratch.extensions.register(new WebSocketExtension());
